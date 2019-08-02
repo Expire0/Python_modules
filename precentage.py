7a8c1c126ccff94c477f4aa1608a195dfb076b42
Mas <airtime166@yahoo.com>
2019-08-02 10:00:44 -0400

    Create precentage.py

---
+++ precentage.py
@@ -0,0 +1,8 @@
+You could just divide your two numbers and multiply by 100. Note that this will throw an error if "whole" is 0, as asking what percentage of 0 a number is does not make sense:
+
+def percentage(part, whole):
+  return 100 * float(part)/float(whole)
+Or if the question you wanted it to answer was "what is 5% of 20", rather than "what percentage is 5 of 20" (a different interpretation of the question inspired by Carl Smith's answer), you would write:
+
+def percentage(percent, whole):
+  return (percent * whole) / 100.0
